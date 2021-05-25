3f319a6b1e15b52bfcce10101a59c9ce2e48f017
Hans-Christian Otto <c.otto@suora.com>
2021-05-25 10:58:54 +0200

    App Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from "react-dom";
 
-const element = <h1>Hallo Bochum und Dortmund!</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+function App() {
+  return <h1>Hallo Visus!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
