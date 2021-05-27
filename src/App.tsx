30c780ed30e9580c467e43ccc7fa0284245bd0ae
Hans-Christian Otto <c.otto@suora.com>
2021-05-27 19:54:33 +0200

    Added support for adding new messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,22 @@ import { useState } from "react";
 import Counter from "./components/Counter";
 import MessageCompose from "./components/MessageCompose";
 import MessageList from "./components/MessageList";
-import MessageView from "./components/MessageView";
 import Toggle from "./components/Toggle";
 import Message from "./domain/Message";
 
 export function App() {
-  const message: Message = {
-    author: "Team Grün",
-    message: "Hallo",
-    id: "huasdadsoj",
-    date: Date.now(),
-  };
-
-  const [messages, setMessages] = useState([
-    { id: "1", author: "Hannah", message: "Hello World", date: Date.now() },
-    { id: "2", author: "Rene", message: "Hallo Team Grün", date: Date.now() },
-    { id: "3", author: "Erdal", message: "Hallo Team Pink", date: Date.now() },
-    { id: "4", author: "Laura", message: "Hallo Bochum", date: Date.now() },
-  ]);
+  const [messages, setMessages] = useState<Message[]>([]);
 
   const handleNewMessage = (text: string) => {
-    console.log(text);
+    setMessages([
+      ...messages,
+      {
+        author: "Luise",
+        id: Date.now().toString(),
+        date: Date.now(),
+        message: text,
+      },
+    ]);
   };
 
   return (
@@ -33,7 +28,6 @@ export function App() {
       </div>
       <MessageList messages={messages} />
       <MessageCompose onNewMessage={handleNewMessage} />
-      <MessageView message={message} />
       <Toggle />
     </div>
   );
