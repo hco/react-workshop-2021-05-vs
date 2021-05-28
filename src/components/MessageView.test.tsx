e9fb7018f0b3e65be96c06a8ac71a3f6b06f3f38
Hans-Christian Otto <c.otto@suora.com>
2021-05-28 09:13:19 +0200

    Added test for messageview

---
+++ src/components/MessageView.test.tsx
@@ -0,0 +1,11 @@
+import Message from "../domain/Message";
+
+const testMessage: Message = {
+  author: "Hannah",
+  date: 1622185877229,
+  id: "huodfasiojadsjiodas",
+  message: "Bald ist Wochenende!",
+};
+describe("", () => {
+  test("", () => {});
+});
